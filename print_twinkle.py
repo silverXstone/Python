463f94a7bec5886f7603c8a802d32e8df9343368
SilverXstone <113440367+silverXstone@users.noreply.github.com>
2022-09-15 11:12:51 +0530

    Update print_twinkle.py

--- print_twinkle.py
+++ print_twinkle.py
@@ -1,6 +1,7 @@
-print("Twinkle, twinkle, little star,")
-print("           How I wonder what you are!")
-print('           ' "Up above the world so high,")
-print("                     Like a diamond in the sky.")
-print("Twinkle, twinkle, little star,")
-print("         How I wonder what you are!")
+  #Write a Python program to print the following string in a specific format.
+print("""Twinkle, twinkle, little star,
+             How I wonder what you are!
+                     Up above the world so high,
+                     Like a diamond in the sky.
+          Twinkle, twinkle, little star,
+               How I wonder what you are!""")
