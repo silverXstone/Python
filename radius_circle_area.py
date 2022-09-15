e4d753f803f7d4be400401b6876a7a46f3f4b832
SilverXstone <113440367+silverXstone@users.noreply.github.com>
2022-09-15 10:36:18 +0530

    Update radius_circle_area.py

--- radius_circle_area.py
+++ radius_circle_area.py
@@ -1,3 +1,4 @@
+#Write a python program which accepts the radius of a circlee from the user and compute the area
 r = int(input("Please enter the radius of the circle:-"))
 a = 3.14*r*r
 print("area of circle:-",a)
