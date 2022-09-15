7df87113e6de8472c35cf97f6de3989229a116b5
SilverXstone <113440367+silverXstone@users.noreply.github.com>
2022-09-15 11:08:12 +0530

    Update examination_schedule.py

--- examination_schedule.py
+++ examination_schedule.py
@@ -1,3 +1,8 @@
+"""Write a Python program to display the examination schedule. (extract the date from 
+exam_st_date).
+exam_st_date = (11, 12, 2022)
+Sample Output: The examination will start from : 11 / 12 / 2022"""
+
 import datetime
 ex = datetime.datetime(2022,11,12)
 print(ex.strftime("%x"))
