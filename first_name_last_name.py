9cbe64eb03639e5b9866ae1267a7bb7fc172fd85
SilverXstone <113440367+silverXstone@users.noreply.github.com>
2022-09-15 11:09:43 +0530

    Update first_name_last_name.py

--- first_name_last_name.py
+++ first_name_last_name.py
@@ -1,3 +1,5 @@
+#Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them
+
 f = input("Enter your first name:-")
 l = input("Enter your last name:-")
 print(l+"  "+f)
